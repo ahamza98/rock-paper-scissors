649eef89a3a4ea49e0a6bc6704c098e00f13e480
ahamza98 <a_hamza98@hotmail.com>
2022-03-08 14:15:40 +0000

    store elements into variables

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+//Setting the user and AI default scores to 0 //
+let userScore = 0;
+let aiScore = 0;
+
+// Storing HTML elements and ID's into variables to access quickly //
+let userScoreDisplay = document.getElementById('userScore');
+let aiScoreDisplay = document.getElementById('aiScore');
+let scores = document.getElementsByClassName('score-area');
+let result = document.getElementById('result');
+// choices //
+let rock = document.getElementById('rock');
+let paper = document.getElementById('paper');
+let scissors = document.getElementById('scissors');
+//reset button//
+let reset = document.getElementById('reset')
+
+
+
+
+
+
+
