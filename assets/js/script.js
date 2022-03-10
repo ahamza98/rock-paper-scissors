d663cf22094fc03f8daa34dd7299ba35bc603374
ahamza98 <a_hamza98@hotmail.com>
2022-03-10 14:35:13 +0000

    display results in results area

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,19 +26,30 @@ let reset = document.getElementById('reset');
     return choices[interger];
 }
 
-/* Actions to screen when user wins, loses or draws (score incrementation and visual represantion
-on the sceen).*/
-function userWin() {
-    userScore++;
-    userScoreDisplay.innerHTML = userScore;
-    aiScoreDisplay.innerHTML = aiScore;
+// convert words to button images on the result section//
+function textToImage(text) {
+    if (text === 'rock') return '<i class="fa-regular fa-hand-back-fist"></i>'
+    if (text === 'paper') return '<i class="fa-regular fa-hand"></i>'
+    if (text === 'scissors') return '<i class="fa-regular fa-hand-scissors"></i>'
 
 }
 
-function aiWin() {
+/* Actions to screen when user wins, loses or draws (score incrementation and visual represantion
+on the sceen).*/
+function userWin(userChoice, aiChoice) {
+    userScore++;
+    userScoreDisplay.innerHTML = userScore;
+    aiScoreDisplay.innerHTML = aiScore;
+    result.innerHTML = textToImage(userChoice)  + " beats " + textToImage(aiChoice) ;
+
+}
+
+function aiWin(userChoice, aiChoice) {
     aiScore++;
     aiScoreDisplay.innerHTML = aiScore;
     userScoreDisplay.innerHTML = userScore;
+    result.innerHTML = textToImage(aiChoice)  + " beats " + textToImage(userChoice) ;
+
 }
 
 function tie() {
@@ -52,15 +63,15 @@ function startGame(userChoice) {
     if ((userChoice === 'rock' && aiChoice === 'scissors')
        || (userChoice === 'paper' && aiChoice === "rock")
        || (userChoice === "scissors" && aiChoice === "paper")) {
-        return userWin();
+        return userWin(userChoice, aiChoice);
     } 
     else if ((userChoice === 'rock' && aiChoice === 'paper')
         || (userChoice === 'paper' && aiChoice === 'scissors')
         || (userChoice === 'scissors' && aiChoice === 'rock')){
-            return aiWin();
+            return aiWin(userChoice, aiChoice);
         }
         else if (userChoice === aiChoice){
-            return tie();
+            return tie(userChoice, aiChoice);
         }
 }
 
